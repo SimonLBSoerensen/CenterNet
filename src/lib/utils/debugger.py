d349a5aac8383ef4b3b1f196bcbba1ba02676ead
Simon Lyck Bjært Sørensen <simso16@student.sdu.dk>
2021-03-26 09:57:48 +0100

    debugger

--- src/lib/utils/debugger.py
+++ src/lib/utils/debugger.py
@@ -194,16 +194,43 @@ class Debugger(object):
       cv2.putText(self.imgs[img_id], txt, (bbox[0], bbox[1] - 2), 
                   font, 0.5, (0, 0, 0), thickness=1, lineType=cv2.LINE_AA)
 
-  def add_coco_hp(self, points, img_id='default'): 
-    points = np.array(points, dtype=np.int32).reshape(self.num_joints, 2)
-    for j in range(self.num_joints):
-      cv2.circle(self.imgs[img_id],
-                 (points[j, 0], points[j, 1]), 3, self.colors_hp[j], -1)
-    for j, e in enumerate(self.edges):
-      if points[e].min() > 0:
-        cv2.line(self.imgs[img_id], (points[e[0], 0], points[e[0], 1]),
-                      (points[e[1], 0], points[e[1], 1]), self.ec[j], 2,
-                      lineType=cv2.LINE_AA)
+  def add_sort_bbox(self, bbox, cat, track, show_txt=True, img_id='default'):
+      bbox = np.array(bbox, dtype=np.int32)
+      # cat = (int(cat) + 1) % 80
+      cat = int(cat)
+      # print('cat', cat, self.names[cat])
+      c = self.colors[cat][0][0].tolist()
+      if self.theme == 'white':
+          c = (255 - np.array(c)).tolist()
+      txt = 'Track: {}'.format(int(track))
+      font = cv2.FONT_HERSHEY_SIMPLEX
+      cat_size = cv2.getTextSize(txt, font, 0.5, 2)[0]
+      cv2.rectangle(
+          self.imgs[img_id], (bbox[0], bbox[1]), (bbox[2], bbox[3]), c, 2)
+      if show_txt:
+          cv2.rectangle(self.imgs[img_id],
+                        (bbox[0], bbox[1] - cat_size[1] - 2),
+                        (bbox[0] + cat_size[0], bbox[1] - 2), c, -1)
+          cv2.putText(self.imgs[img_id], txt, (bbox[0], bbox[1] - 2),
+                      font, 0.5, (0, 0, 0), thickness=1, lineType=cv2.LINE_AA)
+
+  def add_coco_hp(self, points, img_id='default'):
+      points = np.array(points, dtype=np.int32).reshape(self.num_joints, 2)
+      for j in range(self.num_joints):
+          cv2.circle(self.imgs[img_id],
+                     (points[j, 0], points[j, 1]), 3, self.colors_hp[j], -1)
+      for j, e in enumerate(self.edges):
+          if points[e].min() > 0:
+              cv2.line(self.imgs[img_id], (points[e[0], 0], points[e[0], 1]),
+                       (points[e[1], 0], points[e[1], 1]), self.ec[j], 2,
+                       lineType=cv2.LINE_AA)
+
+  def add_fps(self, fps, img_id='default'):
+      txt = '{:.1f}'.format(fps)
+      shape = self.imgs[img_id].shape
+      font = cv2.FONT_HERSHEY_SIMPLEX
+      cv2.putText(self.imgs[img_id], txt, (shape[1] - 40, 20), font, 0.5, (255, 255, 255), thickness=1,
+                  lineType=cv2.LINE_AA)
 
   def add_points(self, points, img_id='default'):
     num_classes = len(points)
